2631c06afd220aab285c8ab51ade9a4c824560ba
Jolyon <jolyon.miles-wilson@blackthrive.org>
2023-02-02 12:22:30 +0000

    add legislation use 2017-21 script and table

---
+++ R/legislation_17-21.R
@@ -0,0 +1,30 @@
+rm(list = ls()) # clean environment
+
+# install and library required packages
+packages <- c('tidyverse','kableExtra')
+pkg_notinstall <- packages[!(packages %in% installed.packages()[,"Package"])]
+lapply(pkg_notinstall, install.packages, dependencies = TRUE)
+lapply(packages, library, character.only = TRUE)
+
+data <- readRDS("./data/ss_records_jan17_dec21.rds")
+
+# count number of cases of each legislation and express as percentage
+legislation <- data %>%
+  group_by(legislation) %>%
+  summarise(
+    n = n()
+  ) %>%
+  mutate(
+    percentage = round(100 * (n / sum(n)),2)
+  )
+
+# make into html table and save
+kable_table <- kable(legislation,
+                     caption = "Stop and search records by legislation, Jan 2017-Dec 2021",
+                     col.names = c("Legislation","Number of stops","Percentage of stops")) %>%
+  kable_styling(full_width = F)
+
+save_kable(kable_table, file = "./outputs/legislation_table.html")
+# save_kable(kable_table, file = "./outputs/legislation_table.pdf")
+# save_kable(kable_table, file = "./outputs/legislation_table.png")
+
